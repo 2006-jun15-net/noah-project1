ce9a7e8e471ac32a0c9216ad6786745d1930a216
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-07-08 11:37:13 -0600

    Fix test project dependency

--- StoreApp/StoreApp.Test/OrderTest.cs
+++ StoreApp/StoreApp.Test/OrderTest.cs
@@ -1,13 +1,11 @@
-﻿using StoreApp.DataAccess.Model;
-using StoreApp.Library.Model;
+﻿using StoreApp.Library.Model;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace StoreApp.Test
 {
-    
+
     public class OrderTest
     {
         Order order = new Order();
